b9d98b1aeed0391e9b70690cbe35bbb142a91d55
EcCo18 <clemens.kurzeya@icloud.com>
2022-09-22 15:31:35 +0200

    init

---
+++ src/main/java/de/eccogaming/isbnservice/IsbnServiceApplication.java
@@ -0,0 +1,13 @@
+package de.eccogaming.isbnservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbnServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbnServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eccogaming/isbnservice/IsbnServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eccogaming.isbnservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbnServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
