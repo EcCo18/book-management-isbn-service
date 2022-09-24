dc2fefac23ca0f5ed8c0091d9d052636a3f6f1ae
EcCo18 <clemens.kurzeya@icloud.com>
2022-09-24 19:30:15 +0200

    refactor(isbn-api): removed unused import

--- src/test/java/de/eccogaming/isbnservice/services/IsbnApiServiceTest.java
+++ src/test/java/de/eccogaming/isbnservice/services/IsbnApiServiceTest.java
@@ -1,6 +1,5 @@
 package de.eccogaming.isbnservice.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
