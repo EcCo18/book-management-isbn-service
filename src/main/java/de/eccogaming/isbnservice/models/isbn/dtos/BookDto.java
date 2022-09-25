e5ddb23fda51b6a6d7c35c6a8d2362ae547e9a9e
EcCo18 <clemens.kurzeya@icloud.com>
2022-09-25 11:38:10 +0200

    refactor(book): changed type of authors to List

---
+++ src/main/java/de/eccogaming/isbnservice/models/isbn/dtos/BookDto.java
@@ -0,0 +1,28 @@
+package de.eccogaming.isbnservice.models.isbn.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookDto {
+
+    private int id;
+
+    @NotNull
+    private List<String> author;
+
+    @Min(1)
+    private int releaseYear;
+
+    @NotNull
+    private String isbn;
+}
